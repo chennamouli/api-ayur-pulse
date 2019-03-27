9370132a524174d46b600ada5d98972a46ddbacf
Chennamouli <mouli.3022@gmail.com>
2019-03-26 19:29:22 -0500

    First commit

---
+++ src/main/java/com/pulse/ayur/ApiAyurPulseApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.ayur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAyurPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAyurPulseApplication.class, args);
+	}
+
+}
